8ca31cf253a1da919f81e60c5e027393db4f27c3
nikita990909 <72967306+nikita990909@users.noreply.github.com>
2022-03-14 14:31:28 +0600

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -1,13 +1,35 @@
 import 'dart:math';
 
 void main() {
-  var str = 14258369;
-  print(numStr(str)+'тг.');
+  double curs = 513;
+  var monthPay = 200000;
+  var month = 12;
+  var procent = 0.2;
+  var procentTxt = procent * 100;
+  var name = 'Попов Никита Станиславович';
+  print(hello(name));
+  print('За $month месяцев вы сможете отложить (учитывая зарплату: ' +
+      numStr(monthPay) +
+      'тг. и процент который будете откладывать: $procentTxt%): \n' +
+      numStr(accumulation(monthPay, month, procent).floor()) +
+      'тг.' +
+      " или ~" +
+      numStr(transferMoney(accumulation(monthPay, month, procent).floor(), curs)
+          .floor()) +
+      "\$");
 }
 
 String hello(String name) => 'Приветствую, $name';
 
 int random(int n) => Random().nextInt(n);
+
+int transferMoney(int money, double curs) {
+  double result = money / curs;
+  return result.floor();
+}
+
+double accumulation(int monthPay, int month, double procent) =>
+    (monthPay * procent) * month;
 
 String numStr(int num) {
   var n = num.toString();
